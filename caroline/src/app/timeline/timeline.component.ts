7f709d03ee476c8a0afd54dfd98314ac0ecb0c6f
CarolineVoo <carrovo98@gmail.com>
2021-05-12 21:24:56 +0200

    Timeline still in progress

--- caroline/src/app/timeline/timeline.component.ts
+++ caroline/src/app/timeline/timeline.component.ts
@@ -15,24 +15,40 @@ export class TimelineComponent implements OnInit {
       {
         year: 1998,
         title: "My birth",
-        content: "I was born in Norway, October 21th 1998. My family is from Vietnam and later moved to Norway in the 1970s. So I grew up in a small town called Askim",
+        content: "I was born in Norway, October 21th 1998. My family is from Vietnam and later moved to Norway in the 1970s. So I grew up in a small town called Askim.",
         img: "../../assets/images/baby-1998-2.png",
         position: 'left',
         end: false
       },
       {
-        year: 2001,
-        title: "",
+        year: 2009,
+        title: "My hobby",
+        content: "I am a physically active person who loves sports. Taekwondo was my first hobby. After 7 years, I had to give up martial arts due to lack of free time.",
+        img: "../../assets/images/hobby-2009.png",
+        position: 'right',
+        end: false
+      },
+      {
+        year: 2014,
+        title: "High School",
         content: "",
-        img: "../../assets/images/baby-1998-2.png",
+        img: "../../assets/images/highschool-2014.png",
+        position: 'left',
+        end: false
+      },
+      {
+        year: 2017,
+        title: "Oslo University",
+        content: "",
+        img: "../../assets/images/oslomet-2017.png",
         position: 'right',
         end: false
       },
       {
         year: 2020,
-        title: "",
+        title: "Capgemini Norway",
         content: "",
-        img: "",
+        img: "../../assets/images/capgemini-2020.png",
         position: 'left',
         end: true
       }
